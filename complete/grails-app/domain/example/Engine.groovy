51a746170f2d16ff11ce980841bf2f976abdeef7
sdelamo <sergio.delamo@softamo.com>
2017-06-26 16:42:48 +0200

    Move logic to common snippets

--- complete/grails-app/domain/example/Engine.groovy
+++ complete/grails-app/domain/example/Engine.groovy
@@ -5,7 +5,6 @@ import grails.gorm.MultiTenant
 class Engine implements MultiTenant<Engine> { // <1>
     Integer cylinders
     String manufacturer  // <2>
-    static belongsTo = [vehicle: Vehicle]
 
     static constraints = {
         cylinders nullable: false
